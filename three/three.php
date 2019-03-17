c1ff40e423ee07059cbdf09c3ce07f40ce957922
lixinyuan <2777893479@qq.com>
2019-03-17 19:33:44 +0800

    edit

---
+++ three/three.php
@@ -0,0 +1,24 @@
+<?php
+
+// 0 1 1 2 3 5 8 13 21
+// function str($n){
+// 	if($n==0){
+// 		return 0;
+// 	}
+// 	if($n==1||$n==2){
+// 		return 1;
+// 	}else{
+// 		return str($n-1)+str($n-2);
+// 	}
+// }
+// echo str(6)
+
+function str($n){
+	$arr = [];
+	$arr[1] = 1;
+	for ($i=$n; $i <$n+1 ; $i++) { 
+		# code...
+	}
+}
+str($n)
+?>
