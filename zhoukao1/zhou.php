aed3c5ccac88f49a8c31d03f9b2a9a596aa0ce59
lixinyuan <2777893479@qq.com>
2019-03-18 09:25:28 +0800

    zhoukao1

---
+++ zhoukao1/zhou.php
@@ -0,0 +1,80 @@
+<?php
+// function str($n){
+// 	$sum = 0;
+// 	for ($i=0; $i < $n; $i++) { 
+// 		$sum = $sum+$i;
+// 	}
+// 	return $sum;
+// }
+// echo  str(101);
+
+
+// $sum = 100;
+// $s = $sum/2*($sum+1);
+// echo $s;	
+
+
+// $sum = 101;
+// $a=0;
+// for ($i=0; $i < $sum; $i++) { 
+// 	$a = $a+$i;
+// }
+// echo $a;
+
+
+//递归
+
+// function str($m){
+// $sum=1;
+// 	if($m==0||$m==1){
+// 		$sum = $m;
+// 	}
+// 	else{
+// 		for ($i=1; $i <$m ; $i++) { 
+
+// 			$sum*=($i+1);
+// 		}
+// 	}
+// 	return $sum;
+// }
+// echo str($m=5);
+
+// 循环
+// $str = 5;
+// $sum=1;
+// for ($i=1; $i < $str; $i++) { 
+// 	$sum = $sum*($i+1);
+// }
+// echo $sum;
+
+// function str($n){
+// 	$sum = strrev($n);
+// 	if($sum == $n){
+// 		echo "是回文字符串";
+// 	}else{
+// 		echo "不是回文字符串";
+// 	}
+// }
+// str('1234');
+
+class Person {
+	public $name;
+	public $age;
+	public function run(){
+
+	}
+}
+implements Walk{
+	function run();
+}
+class Studen{
+	class Studen extends Person implements run(){
+		$obj => ['zhangsan',10];
+		$obj => ['zhangsan',10];
+		$obj => ['zhangsan',10];
+		$obj => ['zhangsan',10];
+		$obj => ['zhangsan',10];
+		print_r($obj);
+	}
+}
+?>
